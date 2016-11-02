e9340d4dd347e3fab237d658695392e8b3dc573a
Kristi Nikolla <knikolla@bu.edu>
2016-11-02 17:34:41 -0400

    Added api unit tests and api to create user

--- setpass.py
+++ setpass.py
@@ -1,14 +1,13 @@
-from flask import Flask
+import json
+import uuid
+
+from flask import Flask, Response
 from flask import request, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
 db = SQLAlchemy(app)
-
-
-class PasswordAlreadyChangedException(Exception):
-    pass
 
 
 class TokenNotFoundException(Exception):
@@ -25,13 +24,11 @@ class User(db.Model):
     user_id = db.Column(db.String(64), unique=True)
     token = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(64), nullable=False)
-    changed = db.Column(db.Boolean, nullable=False)
 
-    def __init__(self, user_id, token, password, changed=False):
+    def __init__(self, user_id, token, password):
         self.user_id = user_id
         self.token = token
         self.password = password
-        self.changed = changed
 
     def __repr__(self):
         return '<User %r, Token %r>' % (self.user_id, self.token)
@@ -39,6 +36,9 @@ class User(db.Model):
     @staticmethod
     def find(token):
         return User.query.filter_by(token=token).first()
+
+
+db.create_all()
 
 
 @app.route('/', methods=['GET'])
@@ -52,9 +52,18 @@ def set_password():
     password = request.form['password']
 
     if not token or not password:
-        raise BadRequestException
+        return Response(response='Not token/password in request', status=400)
 
-    _set_password(token, password)
+    try:
+        _set_password(token, password)
+    except TokenNotFoundException:
+        return Response(response='Token not found', status=404)
+
+    return Response(status=200)
+
+
+def _set_openstack_password(user_id, old_password, new_password):
+    pass
 
 
 def _set_password(token, password):
@@ -64,16 +73,27 @@ def _set_password(token, password):
     if user is None:
         raise TokenNotFoundException
 
-    if user.changed:
-        raise PasswordAlreadyChangedException
+    _set_openstack_password(user.user_id, user.password, password)
 
-    user.changed = True
+    db.session.delete(user)
     db.session.commit()
-    # Login with old_password and set_password in OpenStack to the new one
-    pass
 
 
-db.create_all()
+@app.route('/token/<user_id>', methods=['PUT'])
+def add(user_id):
+    password = request.data
+
+    user = User(
+        user_id=user_id,
+        token=str(uuid.uuid4()),
+        password=password
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return Response(response=user.token, status=200)
+
 
 if __name__ == '__main__':
     app.run()
--- test_setpass.py
+++ test_setpass.py
@@ -7,14 +7,12 @@ setpass.db.create_all()
 
 
 class TestSetpass(object):
-
     @staticmethod
     def create(changed=False):
         user = setpass.User(
             user_id=str(uuid.uuid4()),
             token=str(uuid.uuid4()),
-            password=str(uuid.uuid4()),
-            changed=False
+            password=str(uuid.uuid4())
         )
         setpass.db.session.add(user)
         setpass.db.session.commit()
@@ -32,21 +30,57 @@ class TestSetpass(object):
         yield user
         self.delete(user)
 
-    def test_wrong_token(self, user):
+    @pytest.fixture()
+    def app(self):
+        return setpass.app.test_client()
+
+    def test_internal_wrong_token(self, user):
         wrong_token = 'wrong_token'
         assert wrong_token != user.token
 
         with pytest.raises(setpass.TokenNotFoundException):
             setpass._set_password(wrong_token, 'password2')
 
-    def test_set_password(self, user):
-        assert not user.changed
-
-        # Calling set password sets the changed value to true
+    def test_internal_set_password_twice(self, user):
         setpass._set_password(user.token, 'new_password')
-        assert user.changed
-
-        # You can't change password twice
-        with pytest.raises(setpass.PasswordAlreadyChangedException):
+        with pytest.raises(setpass.TokenNotFoundException):
             setpass._set_password(user.token, 'another_new_password')
 
+    # API Tests
+    def test_add(self, app):
+        # Create a new user
+        user_id = str(uuid.uuid4())
+        password = str(uuid.uuid4())
+        r = app.put('/token/%s' % user_id, data=password)
+        assert r.status_code == 200
+        token = r.data
+
+        # Ensure we get a match
+        user = setpass.User.find(token)
+        assert user.user_id == user_id
+        assert user.password == password
+
+    def test_set_pass(self, app, user):
+        r = app.post('/?token=%s' % user.token, data={'password': 'NEW_PASS'})
+        assert r.status_code == 200
+
+        user = setpass.User.find(token=user.token)
+        assert user is None
+
+    def test_set_password_twice(self, app, user):
+        r = app.post('/?token=%s' % user.token, data={'password': 'NEW_PASS'})
+        assert r.status_code == 200
+
+        r = app.post('/?token=%s' % user.token, data={'password': 'NEW_PASS'})
+        assert r.status_code == 404
+
+    def test_wrong_token(self, app):
+        r = app.post('/?token=%s' % 'WRONG_TOKEN', data={'password': 'NEW_PASS'})
+        assert r.status_code == 404
+
+    def test_no_arguments(self, app):
+        r = app.post('/?token=%s' % 'TOKEN')
+        assert r.status_code == 400
+
+        r = app.post('/', data={'password': 'NEW_PASS'})
+        assert r.status_code == 400
